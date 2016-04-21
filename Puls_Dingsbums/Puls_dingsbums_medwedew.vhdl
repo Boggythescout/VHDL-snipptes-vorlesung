7805c4069d5d050646fc4c0fa43bcfe16fe53c15
Boggy <lukasbockstaller@gmail.com>
2016-04-21 16:22:01 +0200

    Puls_Dingsbums_medwedew Architecture fertig

--- Puls_Dingsbums/Puls_dingsbums_medwedew.vhdl
+++ Puls_Dingsbums/Puls_dingsbums_medwedew.vhdl
@@ -7,7 +7,6 @@ end entity puls;
 
 architecture medwedew of puls is
 begin
-	
 register: process(clk, res_n) is
 		type state_type is (idle, A, B, C);
 		variable state: state_type;
@@ -17,7 +16,37 @@ register: process(clk, res_n) is
 			y<='0';
 		else 
 			if clk='1' and clk'event then 
-
+				case state is
+					when idle => if x='0' then 
+									state := idle;
+									y <= '0';
+								else 
+									state := A;
+									y <= '1';
+								end if;						
+					when A => if x='0' then 
+									state := B;
+									y <= '0';
+								else 
+									state := A;
+									y <= '1';
+								end if;
+					when B => if x='0' then 
+									state := B;
+									y <= '0';
+								else 
+									state := C;
+									y <= '0';
+								end if;
+					when C => if x='0' then 
+									state := idle;
+									y <= '0';
+								else 
+									state := C;
+									y <= '0';
+								end if;
+				end case;
 			end if;
 		end if;
 	end process;
+end architecture;
