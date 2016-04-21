a37b85473b3a60ca048697508eb7db78b3ab9ad3
Boggy <lukasbockstaller@gmail.com>
2016-04-21 16:13:21 +0200

    Puls_Dingsbums_medwedew Architecture grober Aufbau

--- Puls_Dingsbums/Puls_dingsbums_medwedew.vhdl
+++ Puls_Dingsbums/Puls_dingsbums_medwedew.vhdl
@@ -8,3 +8,16 @@ end entity puls;
 architecture medwedew of puls is
 begin
 	
+register: process(clk, res_n) is
+		type state_type is (idle, A, B, C);
+		variable state: state_type;
+	begin
+		if res_n='0' then
+			state:=idle;
+			y<='0';
+		else 
+			if clk='1' and clk'event then 
+
+			end if;
+		end if;
+	end process;
