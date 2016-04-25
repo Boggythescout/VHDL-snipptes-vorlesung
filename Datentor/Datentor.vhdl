0482757b3b0c95854d546a1f41a7f6d23f38052a
Boggy <lukasbockstaller@gmail.com>
2016-04-25 11:24:12 +0200

    Datentor hinzugefügt

---
+++ Datentor/Datentor.vhdl
@@ -0,0 +1,16 @@
+entity datentor is 
+	port(
+		e, g: in std_logic;
+		a: out std_logic
+		);
+end entity Datentor;
+
+
+process(e,g) is
+begin
+	if g='1' then 
+		a<=e;
+	else
+		a<='Z';
+	end if;
+end process;
